47d709c742936254a004192f8b793876042e9e2a
kakeru Aoyama <kakeru0520sou1@gmail.com>
2023-02-17 13:40:16 +0900

    deviseの設定

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,7 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   with_options presence: true do
     validates :name
     validates :status
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     sessions: 'customer/sessions',
     registrations: 'customer/registrations'
   }
-  
+
   root to: 'pages#home'
 
   get '/up/', to: 'up#index', as: :up
